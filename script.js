ba2a7d5291543e2e244d4cccdc6bf52f8343b600
mani-maharaj <manimaharaj288@outlook.com>
2026-05-25 12:56:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// Simple smooth highlight for future expansion.
-// You can leave this file as-is.
-
-console.log("Mani Maharaj personal brand website loaded.");
+console.log("Mani Maharaj personal brand website loaded successfully.");
